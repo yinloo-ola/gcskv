30b27467382a6eafd87fab9070f297e75ca66bb2
tanyinloo <tan.yinloo@olaola.chat>
2021-10-06 15:13:08 +0800

    add more comments

--- gcs.go
+++ gcs.go
@@ -48,6 +48,8 @@ func New(bucket string, basepath string) (GcsStore, error) {
 }
 
 // Get returns the value of the given key. Error is returned if the key is not found
+//
+// add more docs
 func (store GcsStore) Get(key string) ([]byte, error) {
 	ctx := context.Background()
 	key = store.basepath + key
